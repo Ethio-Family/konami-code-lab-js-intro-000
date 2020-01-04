b5918ba2bbd479e5ef2694e7a1de473f9e88fcb3
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-04 05:49:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,53 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+
+function init(){
+let index = 0;
+document.addEventListener(function(event){
+// your code here
+
+
+if (event.key === codes[index]) {
+  index++;
+
+  if (index === codes.length) {
+    alert("Congratualtions!");
+
+    index = 0;
+  }
+} else {
+  index = 0;
 }
+
+
+})
+}
+init()
+
+
+
+// function init() {
+//   // your code here
+//
+// // This is the function that would be invoked by the event listener.
+// function onKeyDownHandler(e) {
+//
+//   const input = document.body.querySelector('input');
+//   const key = e.key;
+//   let index = 0;
+// input.addEventListener('keydown', function(e) {
+//   if (e.key === codes[index]) {
+//     index++;
+//
+//     if (index === codes.length) {
+//       alert("Congratualtions!");
+//
+//       index = 0;
+//     }
+//   } else {
+//     index = 0;
+//   }};
+// }
+// }
+//init()
